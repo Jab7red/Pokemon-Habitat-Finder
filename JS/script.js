6b7a3277fd94b419f66a2a356630c1f3ecbe8a13
Jared Bradley <jaredbradley@jareds-mbp.lan>
2021-12-14 13:13:25 -0600

    Added JS to ensure input functionality

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,39 @@
-
-
 //Variables
-const URL = 'https://pokeapi.co/api/v2/pokemon-habitat/1'
-console.log(URL)
+const link = 'https://pokeapi.co/api/v2/pokemon-habitat/'
+console.log(link)
 
 //Element References
-const $input = $('#input');
+const $input = $('input[type="text"]');
 const $form = $('form');
-const userInput = $input.val()
-
+const userInput = $input.val();
 
 //Event Listeners
 $form.on('submit', handleGetData);
 
 //Functions
-
 function handleGetData(evt) {
     evt.preventDefault();
-    const userInput = $input.val();
+
+    const userInput = $input.val()
     if (!userInput) return;
     $input.val('')
-}
 
-$.ajax(URL + userInput).then(function (data) {
-    console.log(data)
-    render(data);
-}), function (error) {
-    console.log('Somethings wrong')
-    console.log(error);
-}
+    $.ajax(link + userInput).then(function (data) {
+        console.log(data)
+        render(data);
+    }), function (error) {
+        console.log('Somethings wrong')
+        console.log(error);
+    };
 
+};
 function render(habitatData) {
     $('main').html(`
     <h3>Habitat: ${habitatData.name}</h3>
-    <p>Pokemon: ${habitatData.pokemon_species}</p>
+    <p>Pokemon: ${habitatData.pokemon_species}</li>
     `);
-}
+
+};
 
 
 
