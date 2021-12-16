19800e10a1744009d047251be3c71c20ae49e625
Jared Bradley <jaredbradley@jareds-mbp.lan>
2021-12-15 22:04:57 -0600

    Fixed JS code, finalized HTML and CSS

--- JS/script.js
+++ JS/script.js
@@ -19,21 +19,24 @@ function handleGetData(evt) {
     $input.val('')
 
     $.ajax(link + userInput).then(function (data) {
-        console.log(data)
+        data.pokemon_species.forEach(pokemon => {
+            const pokemonName = pokemon.name
+            console.log(pokemonName)
+        });
         render(data);
     }), function (error) {
         console.log('Somethings wrong')
         console.log(error);
     };
 
+
 };
+
 function render(habitatData) {
     $('main').html(`
-    <h3>Habitat: ${habitatData.name}</h3>
-    <p>Pokemon: ${habitatData.pokemon_species}</li>
+    <h3>Habitat: ${habitatData.name} ID #${habitatData.id}</h3>
+    <p>Pokemon: ${habitatData.pokemonName}</p>
     `);
-
 };
 
-
-
+//write a forEach method
